db8a86d9f6e0b0e04bf1cafbf07c75d668b5ac7a
jordan <jharrison1@qa.com>
2023-07-24 12:38:37 +0100

    further classes

--- src/com/qa/classes/Person.java
+++ src/com/qa/classes/Person.java
@@ -2,43 +2,52 @@ package com.qa.classes;
 
 public class Person {
 
+    private static String latinName = "Homo Sapien";
+
     // instance variables
     private String name;
 
     private int age = 1;
 
-    private String hairColour;
+    private final String naturalHairColour;
 
     private String job;
 
     // default constructor - provided by java if you dont create your own
     public Person() {
         super();
+        this.naturalHairColour = "black";
     }
     // overloaded my constructor so I can create a blank person or a full one
 
 
-    public Person(String name, String hairColour, String job) {
-        this(name, 0, hairColour, job); // calls another constructor to save on repeating logic
+    public Person(String name, String naturalHairColour, String job) {
+        this(name, 0, naturalHairColour, job); // calls another constructor to save on repeating logic
     }
 
-    public Person(String name) {
-        super();
-        this.setName(name);
-    }
+//    public Person(String name) {
+//        super();
+//        this.setName(name);
+//    }
 //    public Person(String job) { // cant have two methods with the same signature
 
     // Method used to generate instances of a class
-    public Person(String name, int age, String hairColour, String job) {
+    public Person(String name, int age, String naturalHairColour, String job) {
         super();
         this.setName(name);
         this.setAge(age);
-        this.setHairColour(hairColour);
+        this.naturalHairColour = naturalHairColour;
         this.setJob(job);
     }
 
     public void intro() {
-        System.out.println("Hello, my name is " + name + " and I am a " + age + " years old " + job + " with " + hairColour + " hair.");
+        System.out.println("Hello, my name is " + name + " and I am a " + age + " years old " + job + " with " + naturalHairColour + " hair.");
+    }
+
+    // lets you print the object using sout
+    @Override
+    public String toString() {
+        return "Hello, my name is " + name + " and I am a " + age + " years old " + job + " with " + naturalHairColour + " hair.";
     }
 
     public String getName() {
@@ -61,13 +70,13 @@ public class Person {
         this.age = age;
     }
 
-    public String getHairColour() {
-        return hairColour;
+    public String getNaturalHairColour() {
+        return naturalHairColour;
     }
 
-    public void setHairColour(String hairColour) {
-        this.hairColour = hairColour;
-    }
+//    public void setNaturalHairColour(String naturalHairColour) {
+//        this.naturalHairColour = naturalHairColour;
+//    }
 
     public String getJob() {
         return job;
@@ -75,5 +84,13 @@ public class Person {
 
     public void setJob(String job) {
         this.job = job;
+    }
+
+    public static String getLatinName() {
+        return latinName;
+    }
+
+    public static void setLatinName(String newLatinName) {
+        latinName = newLatinName;
     }
 }
--- src/com/qa/classes/Runner.java
+++ src/com/qa/classes/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
 
         p1.setName("Jordan");
         p1.setAge(29);
-        p1.setHairColour("black");
+//        p1.setNaturalHairColour("black");
         p1.setJob("Trainer");
 
 //        int i;
@@ -17,7 +17,7 @@ public class Runner {
         Person p2 = new Person(); // will instantiate a Person object
         p2.setName("Andy");
         p2.setAge(Integer.MIN_VALUE);
-        p2.setHairColour("brown");
+//        p2.setNaturalHairColour("brown");
         p2.setJob("Trainer");
 
         p1.intro();
@@ -30,9 +30,36 @@ public class Runner {
 
         p3 = null;
         p2.intro();
-        p3.intro();
-
+//        p3.intro(); null pointer here
         p3 = new Person("Aswene", 24, "Black", "Trainer");
         p3.intro();
+// all bad code:
+        System.out.println(p1.getLatinName());
+        System.out.println(p2.getLatinName());
+        System.out.println(p3.getLatinName());
+
+        p1.setLatinName("Homo Superior");
+
+        System.out.println(p1.getLatinName());
+        System.out.println(p2.getLatinName());
+        System.out.println(p3.getLatinName());
+// proper way:
+        System.out.println(Person.getLatinName());
+
+        System.out.println("2 ^ 8: " + Math.pow(2, 8));
+
+        final double gravity = 9.82;
+
+        final Person p4 = new Person("Jordan B", 26,"black", "Trainer");
+        p4.setName("Jordan Benbelaid");
+
+        final int i;
+
+        i = 27;
+
+        System.out.println(i);
+
+        p1.intro();
+        System.out.println(p1);
     }
 }
